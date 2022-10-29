7bc5435c5d309d9ad25b21875241da7c1d367eb7
imridashaikh <74618174+imridashaikh@users.noreply.github.com>
2022-10-29 12:56:05 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+//Paste your firebaseConfig here
+const firebaseConfig = {
+    apiKey: "AIzaSyDGGPK9AD7dRV88xHU3HWvT1L9f4Hvpu8o",
+    authDomain: "ebike-fc9da.firebaseapp.com",
+    projectId: "ebike-fc9da",
+    storageBucket: "ebike-fc9da.appspot.com",
+    messagingSenderId: "219846855107",
+    appId: "1:219846855107:web:c523e83a38fd68aef6d576"
+  };
+  
+//
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
